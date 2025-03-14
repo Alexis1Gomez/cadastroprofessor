0a14b15943f7c36878febafd9e27253d8b902bc6
Alexis1Gomez <ajgrramos5@gmail.com>
2025-03-14 17:39:05 -0400

    progreso 1.0

--- src/app/lista/(main)/page.tsx
+++ src/app/lista/(main)/page.tsx
@@ -14,14 +14,14 @@ const CadastroProfessor = () => {
           <h2 className=" flex  text-2xl font- text-white ">
             Cadastro de Professor
           </h2>
-          <button className="bg-white text-black px-4 py-2 rounded flex items-center">
+          <button  className="bg-white text-black px-4 py-2 rounded flex items-center">
             <FaArrowLeft className="mr-2" /> Voltar
           </button>
         </div>
 
         {/* Table */}
         <div className="flex justify-center items-center ">
-          <div className=" flex items-center justify-center w-[86rem] h-[40rem] bg-[#313056]/25 bg-opacity-30 p-50 shadow-md rounded-4xl ">
+          <div className="flex items-center justify-center w-full max-w-screen-2xl h-full max-h-4xl bg-[#313056]/25 bg-opacity-30 p-50 shadow-md rounded-4xl">
             {/* Formulario */}
             <form className="flex items-center justify-center ml-[-100px]  w-[86rem] h-[30rem] grid grid-cols-2 gap-4">
               {/* Nome do Aluno */}
@@ -30,20 +30,20 @@ const CadastroProfessor = () => {
                   Nome do Professor <span className="text-red-800">*</span>
                 </label>
                 <div className="relative">
-                  <input   type="text" className="w-[30rem] p-3 pl-10 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-900 bg-[#D9D9D9]"/>
+                  <input   type="text" className="w-[40rem] p-3 pl-10  rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-900 bg-[#D9D9D9]"/>
                   <span className="absolute left-3 top-3 text-gray-800"><User className="w-6 h-6 text-[#000]" /></span>
                 </div>
               </div>
 
               {/* Data de Nascimento */}
-              <div>
+              <div className="relative left-52">
                 <label className="text-white font-semibold">
                   Data de Nascimento <span className="text-red-500">*</span>
                 </label>
-                <div className="relative">
+                <div className="">
                   <input
                     type="date"
-                    className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-[#D9D9D9]"
+                    className="w-[20rem]  p-3  rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-[#D9D9D9]"
                   />
                   <span className="absolute left-3 top-3 text-gray-500"></span>
                 </div>
@@ -57,56 +57,58 @@ const CadastroProfessor = () => {
                 <div className="relative">
                   <input
                     type="email"
-                    className="w-[30rem] p-3 pl-10 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-[#D9D9D9]"
+                    className="w-[40rem]  p-3 pl-10  rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-[#D9D9D9]"
                   />
                   <span className="absolute left-3 top-3 text-gray-500"><Mail /></span>
                 </div>
               </div>
 
               {/* Matrícula */}
-              <div>
+              <div className="relative left-52">
                 <label className="text-white font-semibold">Matrícula</label>
                 <div className="relative">
-                  <input className="w-full p-3 border rounded-lg bg-[#D9D9D9]"/>
+                  <input className="w-[20rem] p-3 pl-10  rounded-lg bg-[#D9D9D9]"/>
                   <span className="absolute left-3 top-3 text-gray-500"><GraduationCap /></span>
                 </div>
               </div>
 
               {/* Especialidade */}
-              <div>
-                <label className="text-white font-semibold">
-                  Especialidade <span className="text-red-500">*</span>
-                </label>
-                <select className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500  bg-[#D9D9D9]">
-                  <option>Selecione</option>
-                  <option>Programacão</option>
-                  <option>Banco de datos</option>
-                </select>
-              </div>
-
-              {/* Senha */}
-              <div>
+              <div >
               <label className="text-white font-semibold">
-                  Senha <span className="text-red-500">*</span>
+                  Especialidade <span className="text-red-500">*</span>
                 </label>
                 <div className="relative">
                   <input
                     type="password"
-                    className="w-full p-3 pl-10 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-[#D9D9D9]"
+                    className="w-[30rem] p-3 pl-10  rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-[#D9D9D9]"
+                  />
+                  <span className="absolute left-3 top-3 text-gray-500"><Lock /></span>
+                </div>
+              </div>
+
+              {/* Senha */}
+              <div className="relative top-25 left-25">
+              <label className="text-white font-semibold">
+                  Confirmar senha <span className="text-red-500">*</span>
+                </label>
+                <div className="relative">
+                  <input
+                    type="password"
+                    className="w-[20rem] p-3 pl-10  rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-[#D9D9D9]"
                   />
                   <span className="absolute left-3 top-3 text-gray-500"><Lock /></span>
                 </div>
               </div>
 
               {/* Confirmar Senha */}
-              <div>
+              <div className="relative ">
                 <label className="text-white font-semibold">
-                  Confirmar Senha <span className="text-red-500">*</span>
+                  Senha <span className="text-red-500">*</span>
                 </label>
                 <div className="relative">
                   <input
                     type="password"
-                    className="w-full p-3 pl-10 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-[#D9D9D9]"
+                    className="w-[20rem] p-3 pl-10 border  rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 bg-[#D9D9D9]"
                   />
                   <span className="absolute left-3 top-3 text-gray-500"><Lock /></span>
                 </div>
@@ -114,7 +116,7 @@ const CadastroProfessor = () => {
 
               {/* Botón de Guardar */}
               <div className="col-span-2 flex justify-center mt-4">
-                <button className="bg-green-500 text-white py-3 px-6 rounded-lg font-semibold hover:bg-green-600 transition">
+                <button className="bg-green-500 border border-black text-white py-3 px-20 rounded-lg font-semibold hover:bg-green-600 transition">
                   SALVAR
                 </button>
               </div>
--- src/app/lista/_components/sidebar.tsx
+++ src/app/lista/_components/sidebar.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
-import Dropdown from "@/app/teste/page";
 
 export function SideBar() {
   return (
     <div
-      className="bg-[#313056] text-white p-6 flex flex-col flex-grow items-center font-playfair">
-
+      className="
+      bg-[#313056] text-white p-6 flex flex-col flex-grow items-center font-playfair"
+    >
       <div className="mt-8">
         <Image
           src="/logo_login.png"
@@ -24,8 +24,8 @@ export function SideBar() {
       <nav className="w-full space-y-4">
         <Link
           href={"/dashboard"}
-          className="w-full py-3 bg-[#d9d9d9] hover:bg-[#c0c0c0] rounded-lg flex items-center px-4 gap-3 text-[#313056] transition-all">
-
+          className="w-full py-3 bg-[#d9d9d9] hover:bg-[#c0c0c0] rounded-lg flex items-center px-4 gap-3 text-[#313056] transition-all"
+        >
           <Image
             src="/icon_inicio.png"
             alt="Ícone Início"
@@ -33,10 +33,28 @@ export function SideBar() {
             height={24}
           />
           Início
-
         </Link>
 
-        <Dropdown/>
+        <Link
+          href={"/dashboard/usuarios"}
+          className="w-full py-3 bg-[#d9d9d9] hover:bg-[#c0c0c0] rounded-lg flex items-center justify-between px-4 gap-3 text-[#313056] transition-all"
+        >
+          <div className="justify-between flex flex-row gap-2">
+            <Image
+              src="/icon_usuario.png"
+              alt="Ícone Usuários"
+              width={24}
+              height={24}
+            />
+            Usuários
+          </div>
+          <Image
+            src="/seta.png"
+            alt="icone seta"
+            width={22}
+            height={22}
+          ></Image>
+        </Link>
 
         <button className="w-full py-3 bg-[#d9d9d9] hover:bg-[#c0c0c0] rounded-lg flex items-center px-4 gap-3 text-[#313056] transition-all">
           <Image
@@ -65,9 +83,6 @@ export function SideBar() {
           />
           Curso
         </button>
-
-        
-
       </nav>
     </div>
   );
